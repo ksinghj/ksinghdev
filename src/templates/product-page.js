f29d10fb7452e14e6839e9acf1d35a1739b0508b
ksinghj <kartarsjabanda@gmail.com>
2020-09-18 11:23:28 +0100

    Revert "Update product-page.js"
    This reverts commit d64b272cd5f367f19610d577c6c01ae231646cdf.

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -143,10 +143,24 @@ export const productPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         heading
         description
         intro {
           blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
             text
           }
           heading
@@ -155,6 +169,36 @@ export const productPageQuery = graphql`
         main {
           heading
           description
+          image1 {
+            alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 526, quality: 92) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image2 {
+            alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 526, quality: 92) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image3 {
+            alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 1075, quality: 72) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
         }
         testimonials {
           author
