b57c404bcc09c1c9a7f77655e042d6953f8321f3
Kartar Singh <kartar.jabanda@remarkable.net>
2020-09-22 16:16:46 +0100

    footer style and updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,9 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img src={logo} alt="Kaldi" style={{ width: '10em', height: 'auto' }} />
+          <Link to="/">
+            <img src={logo} alt="Kaldi" style={{ width: '10em', height: 'auto' }} />
+          </Link>
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
@@ -24,21 +26,9 @@ const Footer = class extends React.Component {
                         Home
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
+                    {
+                      // TODO: about page
+                    }
                     <li>
                       <a className="navbar-item" href="/admin/" target="_blank" rel="noopener noreferrer">
                         Admin
