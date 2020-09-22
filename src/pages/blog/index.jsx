5d1ab097cbc303dac6b2875f0dbb8fffc88ab3e9
Kartar Singh <kartar.jabanda@remarkable.net>
2020-09-22 17:12:39 +0100

    blog page styles

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
+import '../../modules/styles/pages/blog.sass'
+
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
@@ -14,17 +16,7 @@ export default class BlogIndexPage extends React.Component {
             backgroundPosition: '50% 52%',
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            My Blog
-          </h1>
+          <h1 className="blog__header has-text-weight-bold is-size-1">My Blog</h1>
         </div>
         <section className="section">
           <div className="container">
