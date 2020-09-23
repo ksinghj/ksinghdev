40fd0eeec936ce8a3c102a3f6ffe1b6e8e273779
ksinghj <kartarsjabanda@gmail.com>
2020-09-23 21:38:35 +0100

    favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,13 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.sass'
 import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from 'gatsby'
+// import { withPrefix } from 'gatsby'
+
+// favicons
+import appleTouch from '../../static/img/apple-touch-icon.png'
+import thirtyTwo from '../../static/img/favicon-32x32.png'
+import sixteen from '../../static/img/favicon-16x16.png'
+import safariPinned from '../../static/img/safari-pinned-tab.svg'
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
@@ -14,11 +20,11 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href={appleTouch} />
+        <link rel="icon" type="image/png" sizes="32x32" href={thirtyTwo} />
+        <link rel="icon" type="image/png" sizes="16x16" href={sixteen} />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="../img/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="mask-icon" href={safariPinned} color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:type" content="business.business" />
